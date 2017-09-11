0019f970fce741a22ddbba624aa727e416f7b46a
Niklas Schoenberger <nik.schoe@icloud.com>
2017-09-11 15:42:47 +0200

    "NEW TOUCH GALLERY"

--- js/ueber.js
+++ js/ueber.js
@@ -103,7 +103,65 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
         };
-        buttonCntBack.addEventListener("click", function () {
+
+        var touchStartCoords =  {'x':-1, 'y':-1}, // X and Y coordinates on mousedown or touchstart events.
+            touchEndCoords = {'x':-1, 'y':-1},// X and Y coordinates on mouseup or touchend events.
+            direction = 'undefined',// Swipe direction
+            minDistanceXAxis = 30,// Min distance on mousemove or touchmove on the X axis
+            maxDistanceYAxis = 30,// Max distance on mousemove or touchmove on the Y axis
+            maxAllowedTime = 1000,// Max allowed time between swipeStart and swipeEnd
+            startTime = 0,// Time on swipeStart
+            elapsedTime = 0,// Elapsed time between swipeStart and swipeEnd
+            targetElement = document.querySelector('.activegaleryimg');// Element to delegate
+
+        function swipeStart(e) {
+            e = e ? e : window.event;
+            e = ('changedTouches' in e)?e.changedTouches[0] : e;
+            touchStartCoords = {'x':e.pageX, 'y':e.pageY};
+            startTime = new Date().getTime();
+            targetElement.textContent = " ";
+        }
+
+        function swipeMove(e){
+            e = e ? e : window.event;
+            e.preventDefault();
+        }
+
+        function swipeEnd(e) {
+            e = e ? e : window.event;
+            e = ('changedTouches' in e)?e.changedTouches[0] : e;
+            touchEndCoords = {'x':e.pageX - touchStartCoords.x, 'y':e.pageY - touchStartCoords.y};
+            elapsedTime = new Date().getTime() - startTime;
+            if (elapsedTime <= maxAllowedTime){
+                if (Math.abs(touchEndCoords.x) >= minDistanceXAxis && Math.abs(touchEndCoords.y) <= maxDistanceYAxis){
+                    direction = (touchEndCoords.x < 0)? 'left' : 'right';
+                    switch(direction){
+                        /*targetElement.textContent = "Left swipe detected";*/
+                        case 'left':
+                            lastImg();
+                            break;
+                            /*targetElement.textContent = "Right swipe detected";*/
+                        case 'right':
+                            nextImg();
+                            break;
+                    }
+                }
+            }
+        }
+
+        function addMultipleListeners(el, s, fn) {
+            var evts = s.split(' ');
+            for (var i=0, iLen=evts.length; i<iLen; i++) {
+                el.addEventListener(evts[i], fn, false);
+            }
+        }
+
+        addMultipleListeners(targetElement, 'mousedown touchstart', swipeStart);
+        addMultipleListeners(targetElement, 'mousemove touchmove', swipeMove);
+        addMultipleListeners(targetElement, 'mouseup touchend', swipeEnd);
+
+
+        var lastImg = function () {
             actualvalue = parseInt(actualvalue);
             if (actualvalue == 0 || actualvalue == -1) {
                 actualvalue = 6;
@@ -130,8 +188,12 @@ document.addEventListener("DOMContentLoaded", function () {
             }, 300);
 
 
-        });
-        buttonCntForw.addEventListener("click", function () {
+        }
+
+        buttonCntBack.addEventListener("click", lastImg);
+
+
+        var nextImg = function () {
 
             if (actualvalue == 7) {
                 actualvalue = -1;
@@ -159,7 +221,10 @@ document.addEventListener("DOMContentLoaded", function () {
             }, 300);
 
 
-        });
+        }
+
+
+        buttonCntForw.addEventListener("click", nextImg);
 
         for (var i = 0; i < galerieelements.length; i++) {//erstelle elemente
 
