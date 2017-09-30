8b8fd8809f76b4100dc2bcb987d37071e08cb165
Niklas Schoenberger <nik.schoe@icloud.com>
2017-09-30 17:21:56 +0200

    "NEW CHANGES"

--- js/onepage.js
+++ js/onepage.js
@@ -5,29 +5,46 @@ window.addEventListener('load', function () {
 
     var getID = getUrl.substring(getUrl.indexOf("#") + 1);
 
+    /*var get_id_el = document.querySelector("#" + getID);
 
-    if (getID.indexOf('to') === 0) {
-        getID = getID.replace("to", "");
-    }
-
-
-    var getLink = document.querySelector(".main__menu__list__item a[data-type=" + '"' + getID + '"]');
+    var getLink = document.querySelector(".main__menu__list__item a[data-type=" + '"' + getID + '"]');*/
 
 
     var links = document.querySelectorAll(".main__menu__list__item a");
 
     var firstItem = document.querySelector(".solo-cnt__item");
-    var scrollRect = firstItem.getBoundingClientRect();
+    var firstItemRect = firstItem.getBoundingClientRect();
 
-    var scrollOff = scrollRect.top + window.scrollY;
 
-    if(window.innerWidth <= 768) {
-        scrollOff = scrollOff / 2.85;
-        console.log("768px");
+    var scrollOff = firstItemRect.top + window.scrollY;
+
+    function checkWidth() {
+        if (window.innerWidth <= 768) {
+            scrollOff = scrollOff / 2.85;
+            console.log(">768px");
+        }
     }
 
-    for (i = 0; i <= links.length - 1; i++) {
 
+    /*TweenLite.to(window, 0.8, {
+        scrollTo: {y: scroll, offsetY: scrollOff},
+        ease: Expo.easeInOut
+    });*/
+
+    window.addEventListener("resize", checkWidth);
+
+
+    var getCurrLink = document.querySelector("[data-type='" + getID + "']");
+
+    var getCurrItem = getCurrLink.parentElement;
+
+
+
+
+    getCurrItem.classList.add("menu__active");
+
+
+    for (i = 0; i <= links.length - 1; i++) {
 
         links[i].addEventListener("click", function (e) {
 
@@ -38,13 +55,10 @@ window.addEventListener('load', function () {
                 var scroll = document.querySelector(href);
 
 
-
                 TweenLite.to(window, 0.8, {
-                    scrollTo: {y: scroll, offsetY: scrollOff},
+                    scrollTo: {y: scroll},
                     ease: Expo.easeInOut
-                })
-
-
+                });
 
 
                 var active = document.querySelector("li.menu__active");
@@ -56,21 +70,55 @@ window.addEventListener('load', function () {
                 if (active) {
                     active.classList.remove("menu__active");
                 }
-
             }
         )
     }
-
-    if (getLink) {
-        setTimeout(function () {
-            getLink.click()
-        }, 250)
-
-    }
+    var solo_cnt = document.querySelectorAll(".solo-cnt__item");
+    var li_links = document.querySelectorAll(".main__menu__list__item");
 
 
-})
-;
+    solo_cnt.forEach(function (t) {
+
+        var waypoint = new Waypoint({
+            element: t,
+            handler: function (direction) {
+
+                var that = this;
+                var upOff;
+
+                function calcOff() {
+                    var newOff = that.options.offset = scrollOff * 50;
+                    return newOff;
+                }
+
+                var old_active = document.querySelector(".menu__active");
+
+                var solo_idx = t.getAttribute("data-idx");
+
+                var li_idx = document.querySelector(".main__menu__list__item[data-idx='" + solo_idx + "']");
+
+                if (direction === "down") {
+                    old_active.classList.remove("menu__active");
+                    li_idx.classList.add("menu__active");
+                }
+
+                if (direction === "up") {
+                    this.options.offset = 0;
+                    old_active.classList.remove("menu__active");
+                    li_idx.previousElementSibling.classList.add("menu__active");
+                }
+
+            },
+            offset: function () {
+                return this.element.clientHeight / 2
+            }
+        });
+
+
+    });
+
+
+});
 
 
 
