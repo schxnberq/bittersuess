3f62aa50cb0676eb2e50dcce3b8ca2d41d946bee
Niklas Schoenberger <nik.schoe@icloud.com>
2017-10-02 04:20:16 +0200

    "UEBER DROPDOWN :NOT(MOBILE)"

--- js/ueber.js
+++ js/ueber.js
@@ -1,36 +1,33 @@
 document.addEventListener("DOMContentLoaded", function (e) {
 
     var ueber_menu = document.querySelector(".ueber-menu__list");
-    var ueber_menuH = ueber_menu.offsetHeight;
+    var ueber_menu_rect = ueber_menu.getBoundingClientRect();
+    var menu_height = ueber_menu_rect.height + 10;
 
-    console.log(ueber_menuH);
-
-    var clicked = false;
-    document.querySelector(".ueber-h1").addEventListener("click", function () {
+    console.log(menu_height);
 
 
-        /*TweenLite.to(document.querySelector(".ueber-menu"), .5, {
-            opacity: 0,
-            visibility: "hidden",
-            height: "0",
-            ease: Expo.easeOut
-        });*/
+    document.querySelector(".ueber-h1").addEventListener("click", function (e) {
+
+
         if(!document.querySelector(".ueber-clicked")) {
             TweenLite.to(document.querySelector(".ueber-menu"), .7, {
                 opacity: 1,
                 visibility: "visible",
-                height: ueber_menuH,
-                classList: "ueber-menu ueber-clicked",
+                height: menu_height,
+                classList: "ueber-menu hiddenOnDesktop ueber-clicked",
                 ease: Power4.easeOut
             });
+            document.querySelector(".ueber-h1 span i").classList.add("clicked");
         } else {
             TweenLite.to(document.querySelector(".ueber-menu"), .7, {
                 opacity: 0,
                 visibility: "hidden",
                 height: 0,
-                classList: "ueber-menu ",
+                classList: "ueber-menu hiddenOnDesktop ",
                 ease: Power4.easeOut
             });
+            document.querySelector(".ueber-h1 span i").classList.remove("clicked");
         }
 
 
